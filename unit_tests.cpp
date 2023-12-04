cd9e508b4e5af46d3a4106054ecc7b86574787b9
brandanallen23 <brandanallen23@vt.edu>
2023-12-04 11:52:30 -0500

    redo

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,2 +1,19 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+#include "binary_tree.hpp"
+
+#include <iostream>
+#include <string>
+#include <functional>
+
+typedef std::string ItemType;
+
+typedef std::function<void(ItemType&)> FunctionType;
+
+TEST_CASE("Test binary tree", "[binary_tree]") {
+    BinaryTree<ItemType, FunctionType> T1;
+    REQUIRE(T1.isEmpty());
+
+
+}
